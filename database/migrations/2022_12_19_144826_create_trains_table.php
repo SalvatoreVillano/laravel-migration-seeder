c7d4c15b418fcf022595298f1ca2093dd6299c15
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-12-19 15:57:01 +0100

    create table "trains" on db

--- database/migrations/2022_12_19_144826_create_trains_table.php
+++ database/migrations/2022_12_19_144826_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione_Partenza');
+            $table->string('Stazione_Arrivo');
+            $table->string('Orario_Partenza');
+            $table->String('Orario_Arrivo');
+            $table->integer('Codice_Treno');
+            $table->integer('Num_Carrozze');
+            $table->tinyInteger('In_Orario');
+            $table->tinyInteger('Cancellato');
             $table->timestamps();
         });
     }
